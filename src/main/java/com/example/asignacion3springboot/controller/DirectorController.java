43993ed73ca57d5ddc2566d5108694d4ba2cea8e
unknown <juansemedina30@gmail.com>
2023-10-17 23:14:53 -0500

    Finished endpoints

--- src/main/java/com/example/asignacion3springboot/controller/DirectorController.java
+++ src/main/java/com/example/asignacion3springboot/controller/DirectorController.java
@@ -1,34 +1,48 @@
 package com.example.asignacion3springboot.controller;
 
+import com.example.asignacion3springboot.model.Director;
+import com.example.asignacion3springboot.repository.DirectorRepository;
+import com.example.asignacion3springboot.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DirectorController {
+    @Autowired
+    MovieRepository movieRepo;
+
+    @Autowired
+    DirectorRepository directorRepo;
 
     @GetMapping("directores")
     public ResponseEntity<?> getDirectores(){
-        return ResponseEntity.status(200).body("directores");
+        var directores = directorRepo.getDirectores();
+        return ResponseEntity.status(200).body(directores);
     }
 
     @GetMapping("director/{id}")
-    public ResponseEntity<?> getDirectorById(@PathVariable String id){
-        return ResponseEntity.status(200).body("Director");
+    public ResponseEntity<?> getDirectorById(@PathVariable long id){
+        var director = directorRepo.getDirectorById(id);
+        return ResponseEntity.status(200).body(director);
     }
 
     @PostMapping("director")
-    public ResponseEntity<?> registerDirector(@PathVariable String id){
-        return ResponseEntity.status(200).body("director");
+    public ResponseEntity<?> registerDirector(@RequestBody Director director){
+        directorRepo.addDirector(director);
+        return ResponseEntity.status(200).body(director);
     }
 
     @PutMapping("director/{id}")
-    public ResponseEntity<?> editDirector(@PathVariable String id){
-        return ResponseEntity.status(200).body("Director");
+    public ResponseEntity<?> editDirector(@PathVariable long id, @RequestBody Director director){
+        directorRepo.update(director);
+        return ResponseEntity.status(200).body(director);
     }
 
     @DeleteMapping("director/{id}")
-    public ResponseEntity<?> deleteDirector(@PathVariable String id){
-        return ResponseEntity.status(200).body("Director");
+    public ResponseEntity<?> deleteDirector(@PathVariable long id){
+        directorRepo.deleteDirector(id);
+        return ResponseEntity.status(200).body("Director deleted");
     }
 
 
--- src/main/java/com/example/asignacion3springboot/controller/MovieController.java
+++ src/main/java/com/example/asignacion3springboot/controller/MovieController.java
@@ -1,5 +1,8 @@
 package com.example.asignacion3springboot.controller;
 
+import com.example.asignacion3springboot.repository.DirectorRepository;
+import com.example.asignacion3springboot.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.asignacion3springboot.model.*;
@@ -8,33 +11,46 @@ import com.example.asignacion3springboot.model.*;
 @RestController
 public class MovieController {
 
+    @Autowired
+    MovieRepository movieRepo;
+
+    @Autowired
+    DirectorRepository directorRepo;
+
     @GetMapping("/peliculas")
     public ResponseEntity<?> getPeliculas(){
-        return ResponseEntity.status(200).body("peliculas");
+        var peliculas = movieRepo.getMovies();
+        return ResponseEntity.status(200).body(peliculas);
     }
 
     @GetMapping("/pelicula/{id}")
-    public ResponseEntity<?> getPeliculaById(@PathVariable String id){
-        return ResponseEntity.status(200).body("pelicula");
+    public ResponseEntity<?> getPeliculaById(@PathVariable long id){
+        var pelicula = movieRepo.getMovieById(id);
+        return ResponseEntity.status(200).body(pelicula);
     }
 
     @PostMapping("pelicula")
     public ResponseEntity<?> createPelicula(@RequestBody Pelicula pelicula){
-        return ResponseEntity.status(200).body("pelicula");
+        movieRepo.saveMovie(pelicula);
+        return ResponseEntity.status(200).body(pelicula);
     }
 
     @PutMapping("pelicula/{id}")
-    public ResponseEntity<?> editPelicula(@PathVariable String id){
-        return ResponseEntity.status(200).body("pelicula");
+    public ResponseEntity<?> editPelicula(@PathVariable long id, @RequestBody Pelicula pelicula){
+        movieRepo.updateMovie(id,pelicula);
+        return ResponseEntity.status(200).body(pelicula);
     }
 
     @DeleteMapping("pelicula/{id}")
-    public ResponseEntity<?> deletePelicula(@PathVariable String id){
-        return ResponseEntity.status(200).body("pelicula");
+    public ResponseEntity<?> deletePelicula(@PathVariable long id){
+        movieRepo.deleteMovie(id);
+        return ResponseEntity.status(200).body("removed succesfully");
     }
 
     @GetMapping("directores/{id}/peliculas")
-    public ResponseEntity<?> getPeliculasByDirectorID(@PathVariable String id){
-        return ResponseEntity.status(200).body("peliculas");
+    public ResponseEntity<?> getPeliculasByDirectorID(@PathVariable long id){
+        var director = directorRepo.getDirectorById(id);
+        var peliculas = movieRepo.listMoviesByDirector(director);
+        return ResponseEntity.status(200).body(peliculas);
     }
 }
